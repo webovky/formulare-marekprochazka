e244b03acee5fc38b1517ae38c9931c46f311550
Marrek Nožka <marek@tlapicka.net>
2019-09-30 07:57:56 +0200

    přejmenování a doplnění šablon

--- startapp.py
+++ startapp.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 from flask import Flask, render_template, session, redirect, request, url_for
 import os
 import functools
@@ -8,34 +9,49 @@ app.secret_key = os.urandom(24)
 
 ############################################################################
 
-slova = ('Super', 'Perfekt', 'Úža', 'Flask')
+slova = ("Super", "Perfekt", "Úža", "Flask")
 
 
 def prihlasit(function):
     @functools.wraps(function)
     def wrapper(*args, **kwargs):
-        if 'user' in session:
+        if "user" in session:
             return function(*args, **kwargs)
         else:
-            return redirect(url_for('login', url=request.path))
+            return redirect(url_for("login", url=request.path))
+
     return wrapper
+
+
 ############################################################################
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('base.html', slova=slova)
+    return render_template("base.html.j2", slova=slova)
 
 
-@app.route('/info/')
+@app.route("/info/")
 def info():
-    return render_template('info.html')
+    return render_template("info.html.j2")
+
 
 @app.route("/abc/")
 def abc():
-    return render_template("abc")
+    return render_template("abc.html.j2")
+
+
+@app.route("/text/")
+def text():
+    return """
+
+<h1>Text</h1>
+
+<p>toto je text</p>
+
+"""
 
 
 ############################################################################
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5555, debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=4444, debug=True)
